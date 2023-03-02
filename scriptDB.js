67a5c5970cbbf46f1938e25451ec89ab29376e09
DidouLbz <119313169+DidouLbz@users.noreply.github.com>
2023-03-02 16:35:10 +0100

    Update scriptDB.js

--- scriptDB.js
+++ scriptDB.js
@@ -1,29 +1,38 @@
-const gems = [
-    {name: 'ruby', price: 10},
-    {name: 'emerald', price: 20},
-    {name: 'diamond', price: 30}
-];
-const store = [
-    {gem: 'ruby', amount: 10},
-    {gem: 'emerald', amount: 12},
-    {gem: 'ruby', amount: 15},
-    {gem: 'diamond', amount: 24}
-];
+var connection = new ActiveXObject("Portfolio-Covid-Project") ;
+
+var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+
+rs.close;
+connection.close; 
+
+
+> INSERT INTO student VALUES (1, 'date', ' date');
+> INSERT INTO student VALUES (2, ' total_cases', 'new_cases');
+> INSERT INTO student VALUES (3, 'total_deaths', 'population');
+> GO
+
+
 const db = window.openDatabase('data', '1.0', 'data', 1*1024*1024);
 db.transaction(t => {
-    t.executeSql('CREATE TABLE gems (name TEXT, price INTEGER)');
-    t.executeSql('CREATE TABLE store (gem TEXT, amount INTEGER)');
+    t.executeSql('CREATE TABLE CovidDeaths (name TEXT, number INTEGER)');
+    t.executeSql('CREATE TABLE PopulationVaccinated  (gem TEXT, amount INTEGER)');
     for (let g of gems) {
-        t.executeSql('INSERT INTO gems (name, price) VALUES (?, ?)',
+        t.executeSql('INSERT INTO CovidDB (total_cases, new_cases,) VALUES (?, ?)',
         [g.name, g.price]);
     }
     for (let s of store) {
-        t.executeSql('INSERT INTO store (gem, amount) VALUES (?, ?)', 
+        t.executeSql('INSERT INTO store (total_deaths, population) VALUES (?, ?)', 
         [s.gem, s.amount]);
     }
-}, e => console.error(e));
-db.transaction(t => t.executeSql(
-    'SELECT g.name, SUM(g.price*s.amount) total FROM gems g ' +
-    'INNER JOIN store s ON g.name = s.gem GROUP BY 1', [],
-    (t, result) => console.log(result.rows)
 ));
